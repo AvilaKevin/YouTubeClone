f2cd6768c579464a72d2767a852eb8ef3f4d8ab6
ViperDex <viperdex2000@hotmail.com>
2023-04-18 19:09:10 -0500

    div comment with buttons and styles

--- YouTubeClone/src/components/Comments.jsx
+++ YouTubeClone/src/components/Comments.jsx
@@ -30,9 +30,14 @@ const Input = styled.input`
     padding: 5px;
     width: 100%;
     color: ${({ theme }) => theme.text};
+
+    :focus{
+        transition: 0.1s;
+        border-bottom: 2px solid white;
+    }
 `;
 
-const Prueba = styled.div`
+const CommentLink = styled.div`
     display: flex;
     align-items: center;
     gap: 10px;
@@ -44,12 +49,64 @@ const StyledLink = styled(Link)`
     text-decoration: none;
 `;
 
+const FocusComment = styled.div`
+    width: 100%;
+    padding: 5px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const BComment = styled.button`
+    width: 86px;
+    height: 36px;
+    padding: 6px;
+    cursor: pointer;
+    color: ${({ theme }) => theme.text};
+    border: none;
+    border-radius: 18px;
+    text-decoration: none;
+    font-size: 14px;
+    background-color: #3ea6ff;    
+
+    :disabled {
+        background-color: #717171;
+        opacity: 0.3;
+        cursor: default;
+    }
+`;
+
+const BCancel = styled.button`
+    width: 74px;
+    height: 36px;
+    padding: 6px;
+    cursor: pointer;
+    color: ${({ theme }) => theme.text};
+    background-color: transparent;
+    border: none;
+    border-radius: 18px;
+    text-decoration: none;
+    font-size: 14px;
+    font-weight: bolder;
+    margin-right: 8px;
+
+    :hover{
+        border-radius: 18px;
+        background-color: #3f3f3f;
+    }
+`;
+
 function Comments({ videoId }) {
     // Para acceder a las propiedades de nuestro estado, se hace uso de useSelector, en este caso se esta accediendo a currentUser que esta dentro de user para extraer algunas de sus propiedades
     const { currentUser } = useSelector((state) => state.user);
 
     // Se crean los estados para almacenar la informacion que se va a extraer del servidor
     const [comments, setComments] = useState([]);
+
+    // Se crea un estado que muestra el div
+    const [showDiv, setShowDiv] = useState(false);
+
+    // Se crea un estado q habilita el boton cuando se esccribe algo en el input
+    const [inputValue, setInputValue] = useState('');
 
     // El useEffect se ejecutara cada vez que se cambie de id de video
     useEffect(() => {
@@ -68,28 +125,56 @@ function Comments({ videoId }) {
 
     }, [videoId]);
 
+    // Cuando se da click en el input se muestra el div
+    const handleInput = () => {
+        setShowDiv(true);
+    };
+
+    // Cuando se escribe algo en el input se habilita el boton
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    // Cierra el input
+    const handleBCancel = () => {
+        setShowDiv(false);
+    };
+
+    const handleComment = async () => {
+
+    };
+
     return (
         <Container>
             <NewComment>
                 {currentUser ?
                     <>
                         <Avatar src={currentUser.img} />
-                        <Input placeholder='Add a comment...' />
+                        <Input type="text" onChange={handleInputChange} onClick={handleInput} placeholder='Add a comment...' />
                     </>
                     :
                     <StyledLink to="/Signin">
-                        <Prueba>
+                        <CommentLink>
                             <Avatar src={"https://yt3.ggpht.com/a/default-user=s48-c-k-c0x00ffffff-no-rj"} />
                             <Input placeholder='Add a comment...' />
-                        </Prueba>
+                        </CommentLink>
                     </StyledLink>}
             </NewComment>
+            {/* Si showDiv es true entonces se renderiza el componente, de lo contrario no se renderiza nada */}
+            {showDiv && <FocusComment>
+                <div></div>
+                <div>
+                    <BCancel onClick={handleBCancel}>Cancel</BCancel>
+                    <BComment onClick={handleComment} disabled={!inputValue}>Comment</BComment>
+                </div>
+            </FocusComment>
+            }
             {
                 comments.map(comment => (
                     <Comment key={comment._id} comment={comment} />
                 ))
             }
-        </Container >
+        </Container>
     )
 };
 
